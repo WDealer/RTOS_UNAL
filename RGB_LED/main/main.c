096612e759056872cb98a7a2e030aa19546042de
ALEJANDRO\vayne <alsanchezsa@unal.edu.co>
2023-03-14 23:50:08 -0500

    Feat: RGB led was refactor in order to make fade while changing led status

--- RGB_LED/main/main.c
+++ RGB_LED/main/main.c
@@ -1,142 +1,114 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <esp_log.h>
-#include <driver/gpio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/queue.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
 
-//Define RGB LED pins
-#define LED_R_PIN 26
-#define LED_G_PIN 27
-#define LED_B_PIN 14
+#define LED_RED_CHANNEL           LEDC_CHANNEL_0
+#define LED_GREEN_CHANNEL         LEDC_CHANNEL_1
+#define LED_BLUE_CHANNEL          LEDC_CHANNEL_2
+#define LEDC_TEST_CH_NUM       (3)
 
-// RGB LED init function
+#define LED_RED_PIN 18
+#define LED_GREEN_PIN 19
+#define LED_BLUE_PIN 21
+#define INTERRUPTION_BUTTON 26
 
-//Definie Interruption Pin
+/*Ledc timer configuration*/
 
-#define pin_INTERUPTION_1 15
+ledc_timer_config_t ledc_timer = {
+    .duty_resolution = LEDC_TIMER_13_BIT, 
+    .freq_hz = 5000,                      
+    .speed_mode = LEDC_LOW_SPEED_MODE,           
+    .timer_num = LEDC_TIMER_0            
+};
 
-// Define queue variable
-QueueHandle_t inte_queue;
+/*Ledc colors channels configuration*/
 
-//button init
-
-void int_button_init()
-{
-    gpio_set_direction(pin_INTERUPTION_1, GPIO_MODE_INPUT);
-    gpio_pulldown_en(pin_INTERUPTION_1);
-    gpio_pullup_dis(pin_INTERUPTION_1);
-    gpio_set_intr_type(pin_INTERUPTION_1, GPIO_INTR_POSEDGE);
-
-}
-
-//rgb led init
-
-void rgb_led_init()
-{
-
-    //set red LED GPIO
-    //gpio_pad_select_gpio(LED_R_PIN);
-    gpio_set_direction(LED_R_PIN,GPIO_MODE_OUTPUT);
-
-    //set green LED GPIO
-    //gpio_pad_select_gpio(LED_G_PIN);
-    gpio_set_direction(LED_G_PIN,GPIO_MODE_OUTPUT);
-
-    //set blue LED GPIO
-    //gpio_pad_select_gpio(LED_B_PIN);
-    gpio_set_direction(LED_B_PIN,GPIO_MODE_OUTPUT);
-
-}
-
-//RGB LED Color function
-void rgb_led_set_color(uint8_t r, uint8_t g, uint8_t b)
-{
-    gpio_set_level(LED_R_PIN, r);
-    gpio_set_level(LED_G_PIN, g);
-    gpio_set_level(LED_B_PIN, b);
-}
-
-//Interruption rutine
-void IRAM_ATTR gpio_isr_handler(void *arg)
-{
-    int inte_value = (int)arg;
-    xQueueSendFromISR(inte_queue, &inte_value, NULL);
-}
-
-//led_pattern_enum
-typedef enum led_pattern_t{
-    LED_OFF,
-    LED_RED,
-    LED_GREEN,
-    LED_BLUE,
-    LED_TOTAL
-} led_pattern_t;
-
-//interruption Task
-
-void buttonPushedTask(void *arg)
-{    
-    int inte_value;
-
-    while(true)
+ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
     {
-        if(xQueueReceive(inte_queue,&inte_value,portMAX_DELAY))
+        .channel    = LED_RED_CHANNEL,
+        .duty       = 0,
+        .gpio_num   = LED_RED_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_sel  = LEDC_TIMER_0
+    },
+    {
+        .channel    = LED_GREEN_CHANNEL,
+        .duty       = 0,
+        .gpio_num   = LED_GREEN_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_sel  = LEDC_TIMER_0
+    },
+    {
+        .channel    = LED_BLUE_CHANNEL,
+        .duty       = 0,
+        .gpio_num   = LED_BLUE_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_sel  = LEDC_TIMER_0
+    },
+};
+
+void rgb_button_task(void *arg)
+{
+    uint8_t button_status = 0;
+    uint8_t color_index = 0;
+    while(1)
+    {
+        button_status = gpio_get_level(INTERRUPTION_BUTTON);
+        if(button_status == 0)
         {
-            //disable interrupt
-            gpio_isr_handler_remove(inte_value);
-
-            //Wait while button is released
-
-            do
+            color_index++;
+            if(color_index > 2)
             {
-                vTaskDelay(20 / portTICK_PERIOD_MS);
-            } while (gpio_get_level(inte_value)==1);
-
-            //switch case to changue RGB color
-
-            static led_pattern_t pattern_index = 0;
-            pattern_index++;
-            pattern_index %= LED_TOTAL;
-            
-            switch (pattern_index)
+                color_index = 0;
+            }
+            switch(color_index)
             {
-                case LED_OFF:
-                    rgb_led_set_color(0,0,0);
+                case 0:
+                /*ledc_fade_func used to set red led duty cycle*/
+                ledc_set_fade_time_and_start(ledc_channel[LED_RED_CHANNEL].speed_mode,ledc_channel[LED_RED_CHANNEL].channel,0,500,LEDC_FADE_WAIT_DONE);
+                ledc_set_fade_time_and_start(ledc_channel[LED_RED_CHANNEL].speed_mode,ledc_channel[LED_RED_CHANNEL].channel,1024,500,LEDC_FADE_WAIT_DONE);
+                ledc_set_fade_time_and_start(ledc_channel[LED_RED_CHANNEL].speed_mode,ledc_channel[LED_RED_CHANNEL].channel,0,500,LEDC_FADE_WAIT_DONE);            
                     break;
-                case LED_RED:
-                    rgb_led_set_color(1,0,0);
+
+                case 1:
+                /*ledc_fade_func used to set green led duty cycle*/
+                ledc_set_fade_time_and_start(ledc_channel[LED_GREEN_CHANNEL].speed_mode,ledc_channel[LED_GREEN_CHANNEL].channel,0,500,LEDC_FADE_WAIT_DONE);
+                ledc_set_fade_time_and_start(ledc_channel[LED_GREEN_CHANNEL].speed_mode,ledc_channel[LED_GREEN_CHANNEL].channel,1024,500,LEDC_FADE_WAIT_DONE);
+                ledc_set_fade_time_and_start(ledc_channel[LED_GREEN_CHANNEL].speed_mode,ledc_channel[LED_GREEN_CHANNEL].channel,0,500,LEDC_FADE_WAIT_DONE);
                     break;
-                case LED_GREEN:
-                    rgb_led_set_color(0,1,0);
-                    break;
-                case LED_BLUE:
-                    rgb_led_set_color(0,0,1);
-                    break;
-                default:
+
+                case 2:
+                /*ledc_fade_func used to set blue led duty cycle*/
+                ledc_set_fade_time_and_start(ledc_channel[LED_BLUE_CHANNEL].speed_mode,ledc_channel[LED_BLUE_CHANNEL].channel,0,500,LEDC_FADE_WAIT_DONE);
+                ledc_set_fade_time_and_start(ledc_channel[LED_BLUE_CHANNEL].speed_mode,ledc_channel[LED_BLUE_CHANNEL].channel,1024,500,LEDC_FADE_WAIT_DONE);
+                ledc_set_fade_time_and_start(ledc_channel[LED_BLUE_CHANNEL].speed_mode,ledc_channel[LED_BLUE_CHANNEL].channel,0,500,LEDC_FADE_WAIT_DONE);
                     break;
             }
-            // Re-enable the interrupt
-            gpio_isr_handler_add(inte_value, gpio_isr_handler, (void *)inte_value);
-            
-
+            vTaskDelay(100 / portTICK_PERIOD_MS);
         }
     }
 }
 
-
-void app_main(void)
+void app_main()
 {
-    int_button_init();
-    rgb_led_init();
+    /*Install ledc fade function in order to used in rgb task*/
+    ledc_fade_func_install(0);
+    
+    /*Button pin intiallization*/
+    gpio_pad_select_gpio(INTERRUPTION_BUTTON);
+    gpio_set_direction(INTERRUPTION_BUTTON, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(INTERRUPTION_BUTTON, GPIO_PULLUP_ONLY);
 
-    inte_queue = xQueueCreate(10, sizeof(int));
-    xTaskCreate(buttonPushedTask, "buttonPushedTask", 2048, NULL, 1, NULL);
 
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(pin_INTERUPTION_1, gpio_isr_handler, (void *)pin_INTERUPTION_1);
+    /*Ledc init*/
+    ledc_timer_config(&ledc_timer);
+    for(int ch = 0; ch < LEDC_TEST_CH_NUM; ch++)
+    {
+        ledc_channel_config(&ledc_channel[ch]);
+    }
 
-
+    /*Task*/
+    xTaskCreate(rgb_button_task, "rgb_button_task", 2048, NULL, 10, NULL);
 }
